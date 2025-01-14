c71dd1881b327775f44bcc98bcde816a8949911a
gaurav031 <gauravraaz2005@gmail.com>
2025-01-14 21:36:55 +0530

    change the file

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    // origin:'http://localhost:5173',
+    origin:'https://best-match-freelancer.netlify.app/',
     credentials:true
 }
 
